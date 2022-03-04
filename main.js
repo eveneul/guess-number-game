4eccd1375384cdb1efb0bbc5d0f0cf9b609e6e0c
오하늘 <nebulass@naver.com>
2022-03-04 16:15:30 +0900

    Second commit

--- main.js
+++ main.js
@@ -15,6 +15,11 @@ function randomNum () {
 
 function play () {
   const userNum = parseInt(inputEl.value);
+
+  if (userNum < 1 || userNum > 100) {
+    resultText.textContent = '1부터 100 사이 숫자만 입력해 주세요';
+    return
+  }
   
   if (numHistory.includes(userNum)) {
     resultText.textContent = '중복된 숫자입니다 다른 숫자를 입력해 주세요';
@@ -27,16 +32,22 @@ function play () {
   if (userNum == pickRandomNum) {
     resultText.textContent = '정답!';
     gameBtnEl.disabled = true;
+    gameBtnEl.style.backgroundColor = '#84b5ff'
   } else if (userNum < pickRandomNum) {
-    resultText.textContent = 'Down!';
-  } else {
     resultText.textContent = 'Up!';
+  } else {
+    resultText.textContent = 'Down!';
   }
 
   numHistory.push(userNum);
 
-  if (chance == 0) {
+  if (chance < 1) {
+    gameOver = true;
+  }
+
+  if (gameOver == true) {
     gameBtnEl.disabled = true;
+    gameBtnEl.style.backgroundColor = '#84b5ff'
   }
 }
 
@@ -45,11 +56,12 @@ function gameReset () {
   resultText.textContent = '';
   chanceText.textContent = '';
   inputEl.value = '';
-  gameBtnEl.disabled = false;
   gameOver = false;
+  gameBtnEl.disabled = false;
+  gameBtnEl.style.backgroundColor = '#0066ff'
   numHistory = [];
   chance = 5;
-  chanceText.textContent = `${chance}번!`
+  chanceText.textContent = '';
 }
 
 randomNum();
